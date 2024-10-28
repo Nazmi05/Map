610780be8017a15b567ccdac0da9d03a5a770497
Nazmi05 <narikoscarlet@gmail.com>
2024-10-28 17:04:42 +0600

    Add files via upload

---
+++ hw-Map/main.js
@@ -0,0 +1,63 @@
+
+/*const task1=function(){
+const double=[1, 2, 3]
+
+const mapdouble=double.map(item=>{
+        return item*2;
+})
+    
+console.log(mapdouble);
+}
+*/
+
+/*const task2=function(){
+const upper= ['привет', 'мир']
+
+const mapupper=upper.map(item=>{
+    return item.toUpperCase()
+})
+
+console.log(mapupper);
+}*/
+
+/*const task3=function(){
+const num=[1, 2, 3, 4]
+
+const string=num.map(item=>{
+      return item+'';
+  })
+
+
+console.log(string);
+}
+*/
+
+/*const task4=function(){
+const exc=['кот', 'собака', 'тигр']
+
+const withexc=exc.map(item=>{
+    return item+"!";
+})
+
+console.log(withexc);
+}
+
+*/
+/*
+const task5=function(){
+const people = [
+      { name: "John", age: 17 },
+      { name: "Jane", age: 22 },
+      { name: "Mark", age: 19 },
+      { name: "Anna", age: 16 },
+      { name: "Bob", age: 24 }
+    ];
+
+const peopleN=people.map(item=>{
+    return item.name;
+})
+
+console.log(peopleN);
+
+}
+*/
